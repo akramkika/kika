f296896246dd9f14af7320a121d0b433698d89d4
theohugues <theo.hugues@gmail.com>
2017-05-15 15:36:56 +0200

    com

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-
+  // dm=directmessage
   // If message is hello, post hello too
   if (msg.content === 'hello') {
     msg.channel.sendMessage('Hello to you too, fellow !')
