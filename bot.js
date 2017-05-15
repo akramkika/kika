bb039f077cef6e3249f63d7cbfb096f76a4be0f5
Al-Eiks <alexis.santangeli@gmail.com>
2017-05-15 17:00:16 +0200

    Test POST tweet

--- bot.js
+++ bot.js
@@ -1,6 +1,20 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+var Twitter = require('twitter')
+
+var twitterclient = new Twitter({
+  consumer_key: '2i0pUPgNow0UxEfqaRLAoSpdZ',
+  consumer_secret: 'tOesWjSJN5IlspYu9oPMMJN8q8qdu0gYffZkx1lRjnE5rfcuIK',
+  access_token_key: '864107579680731136-bXEcBf0JwcY0mxvMyreu0jAd6spn3rJ',
+  access_token_secret: 'eDX69eMS9RoWkUnIRpOsoKeZnT8W1qhyoiiJHrgBvw0Kb'
+})
+
+twitterclient.post('statuses/update', {status: 'I Love Twitter'}, function (error, tweet, response) {
+  if (error) throw error
+  console.log(tweet)
+  console.log(response)
+})
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -16,7 +30,5 @@ client.on('message', msg => {
     msg.channel.sendMessage('Hello to you too, fellow !')
   }
 })
-
-ddddd
 
 client.login(config.token)
