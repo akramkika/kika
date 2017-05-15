befdf3a633fb6d8dedd5dae5ab78fde2cf769c90
theohugues <theo.hugues@gmail.com>
2017-05-15 14:13:00 +0200

    configure token

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEzNjQ2ODI4ODk1ODYyNzg0.C_srsg.kFa5FV_2ZMvwbq4l_QbLtpHaOJk',
   channel: 'channelid'
 }
