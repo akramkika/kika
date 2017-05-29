bb85aac3908e57c43871fa525f4dc5da5cc92a1c
theohugues <theo.hugues@gmail.com>
2017-05-30 00:42:10 +0200

    google translate bot with language options

--- bot.js
+++ bot.js
@@ -35,6 +35,8 @@ client.on('message', msg => {
       var a = (msg.content).length
       var b = a - 3
       var ask = msg.content.substring(9, b)
+    } else {
+      ask = msg.content.substring(9)
     }
 
     msg.channel.sendMessage('Hello, you ask for: ' + ask)
@@ -53,6 +55,29 @@ client.on('message', msg => {
     })
     msg.channel.sendMessage('Finish your research with --v --c --p to search for video channel playlist')
   }
-})
 
+  if (msg.content.startsWith('!translate ')) {
+    var googleTranslate = require('google-translate')('AIzaSyBqCmlXiZ1db_57J3LSqpaIFF-7B8DElLQ')
+    var txt = msg.content.substring(11)
+    if ((txt).includes('--')) {
+      var w = (msg.content.substring(0)).lastIndexOf('--')
+      var n = w + 2
+      var m = n + 2
+      var lang = msg.content.substring(n, m)
+      txt = msg.content.substring(11, w)
+    } else {
+      lang = 'en'
+      txt = msg.content.substring(11)
+    }
+    googleTranslate.translate(txt, lang, function (err, translation) {
+      if (err) {
+        console.log(err)
+      } else {
+        msg.channel.send(translation.translatedText)
+        // =>  Mi nombre es Brandon
+      }
+    })
+    msg.channel.sendMessage('Finish your research with --*language in abbreviation* (ex: --en for english) to translate in a specified language ')
+  }
+})
 client.login(config.token)
