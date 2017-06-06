4f29505a4863f64a673ea78e4dc0be6368072821
Al-Eiks <alexis.santangeli@gmail.com>
2017-06-06 18:44:16 +0200

    Séparation du code + fonction tweet

--- bot.js
+++ bot.js
@@ -1,20 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
-var Twitter = require('twitter')
-
-var twitterclient = new Twitter({
-  consumer_key: '2i0pUPgNow0UxEfqaRLAoSpdZ',
-  consumer_secret: 'tOesWjSJN5IlspYu9oPMMJN8q8qdu0gYffZkx1lRjnE5rfcuIK',
-  access_token_key: '864107579680731136-bXEcBf0JwcY0mxvMyreu0jAd6spn3rJ',
-  access_token_secret: 'eDX69eMS9RoWkUnIRpOsoKeZnT8W1qhyoiiJHrgBvw0Kb'
-})
-
-twitterclient.post('statuses/update', {status: 'I Love Twitter'}, function (error, tweet, response) {
-  if (error) throw error
-  console.log(tweet)
-  console.log(response)
-})
+var twitter = require('./twitter.js')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -29,6 +16,8 @@ client.on('message', msg => {
   if (msg.content === 'hello') {
     msg.channel.sendMessage('Hello to you too, fellow !')
   }
+  // permet de tweeter avec l'utilisation de "!tweet"
+  twitter.sendTweet(msg)
 })
 
 client.login(config.token)
