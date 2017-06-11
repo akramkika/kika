2d6b370abde5f24c2d17f798f4703c17dc9bf526
theohugues <theo.hugues@gmail.com>
2017-06-11 12:54:29 +0200

    modified bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
 var youtube = require('./youtube.js')
+var translate = require('./translate.js')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -19,5 +20,8 @@ client.on('message', msg => {
 
   // permet d'effectuer une recherche youtube
   youtube.searchYoutube(msg)
+
+  // permet de traduire un message
+  translate.translate(msg)
 })
 client.login(config.token)
