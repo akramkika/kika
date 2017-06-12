e8cdb1644e83892e616d759ecf9f5da88d8fffad
Jeremy Gourou <JGOU@telindus.intra>
2017-06-13 01:51:42 +0200

    Ajout Commande + Filtre

--- spotify.js
+++ spotify.js
@@ -6,43 +6,85 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: clientSecret
 })
 var result
+var type
 
 module.exports = {
   spotify: function (msg) {
     if (msg.content.lastIndexOf('!spotify') !== -1) {
       spotifyApi.clientCredentialsGrant()
+
+
+
       // TOKEN
       .then(function (data) {
+
         console.log(' Le token expire le : ' + data.body['expires_in'])
         console.log('Voici le token : ' + data.body['access_token'])
+
         spotifyApi.setAccessToken(data.body['access_token'])
         result = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
-        // ARTISTES
-        spotifyApi.searchArtists(result)
-        .then(function (data) {
-          msg.channel.sendMessage('ARTISTES - Résultats de votre recherche pour "' + result + '"')
-          for (var i = 0; i < 3; i++) {
-            msg.channel.sendMessage('"' + data.body.artists.items[i].name + '"')
-          }
-        })
-        // ALBUMS
-        spotifyApi.searchTracks('album:' + result)
-        .then(function (data) {
-          msg.channel.sendMessage('ALBUMS - Résultats de votre recherche pour "' + result + '"')
-          for (var i = 0; i < 3; i++) {
-            msg.channel.sendMessage('"' + data.body.tracks.items[i].album.name + '" de ' + data.body.tracks.items[i].artists[0].name)
-          }
-        })
 
-        // TRACKS
-        spotifyApi.searchTracks('track:' + result)
-        .then(function (data) {
-          msg.channel.sendMessage('TRACKS - Résultats de votre recherche pour "' + result + '"')
-          for (var i = 0; i < 3; i++) {
-            msg.channel.sendMessage('"' + data.body.tracks.items[i].name + '" de ' + data.body.tracks.items[i].artists[0].name)
-          }
+        if (msg.content.endsWith('--alb')) {
+          type = 0
+        }
+        else if (msg.content.endsWith('--art')) {
+          type = 1
+        }
+        else if (msg.content.endsWith('--tra')) {
+          type = 2
+        }
+        else {
+          type = 2
+        }
+
+        if (msg.content.endsWith('--art') || msg.content.endsWith('--tra') || msg.content.endsWith('--alb')) {
+           var a = (msg.content).length
+           var b = a - 5
+           var asw = msg.content.substring(9, b)
+        }
+        else {
+           asw = msg.content.substring(9)
+        }
+
+      if (type == 0) {
+          // ALBUMS
+          spotifyApi.searchTracks('album:' + asw)
+          .then(function (data) {
+            msg.channel.sendMessage('ALBUMS - Résultats de votre recherche pour "' + asw + '"')
+            for (var i = 0; i < 3; i++) {
+              msg.channel.sendMessage('"' + data.body.tracks.items[i].album.name + '" de ' + data.body.tracks.items[i].artists[0].name)
+            }
         })
+}
+        if (type == 1) {
+          // ARTISTES
+          spotifyApi.searchArtists(asw)
+          .then(function (data) {
+            msg.channel.sendMessage('ARTISTES - Résultats de votre recherche pour "' + asw + '"')
+            for (var i = 0; i < 3; i++) {
+              msg.channel.sendMessage('"' + data.body.artists.items[i].name + '"')
+            }
+          })
+        }
+          if (type == 2) {
+
+          // TRACKS
+          spotifyApi.searchTracks('track:' + asw)
+          .then(function (data) {
+            msg.channel.sendMessage('TRACKS - Résultats de votre recherche pour "' + asw + '"')
+            for (var i = 0; i < 3; i++) {
+              msg.channel.sendMessage('"' + data.body.tracks.items[i].name + '" de ' + data.body.tracks.items[i].artists[0].name)
+            }
+          })
+        }
+
+
+
+msg.channel.sendMessage('Vous pouvez affinez votre recherche avec --alb --art --tra pour rechercher un album, un artiste ou une track.')
+
       })
+
     }
+
   }
 }
