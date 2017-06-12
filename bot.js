6c3064e1b3e7d3d8c002715933aebb9970aad171
Jeremy Gourou <JGOU@telindus.intra>
2017-06-12 18:27:26 +0200

    bot spotify + type de recherche

--- bot.js
+++ bot.js
@@ -12,11 +12,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-  // dm=directmessage
-  // If message is hello, post hello too
-  if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hello to you too, fellow !')
+  if (msg.content.lastIndexOf('!spotify') !== -1) {
   }
   // permet d'effectuer une recherche youtube
   youtube.searchYoutube(msg)
