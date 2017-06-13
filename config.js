345fa478fdfd86f4cfb20e53ed2406ccbbef3612
akramkika <akramkika@users.noreply.github.com>
2017-06-14 00:44:57 +0200

    Create config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   consumer_secret: 'tOesWjSJN5IlspYu9oPMMJN8q8qdu0gYffZkx1lRjnE5rfcuIK',
   access_token_key: '864107579680731136-bXEcBf0JwcY0mxvMyreu0jAd6spn3rJ',
   access_token_secret: 'eDX69eMS9RoWkUnIRpOsoKeZnT8W1qhyoiiJHrgBvw0Kb',
-  apiKey: 'AIzaSyBuXYDRQ20gsN66_WMCTokknoxWHiTYJZQ',
+  youtubeKey: 'AIzaSyBuXYDRQ20gsN66_WMCTokknoxWHiTYJZQ',
   clientId: '132f44ff456c41d29d544a608b448bbd',
   clientSecret: '25d2c3ee445140bfa9aaa3d6bcdc194b',
   owm_token: 'd3d081b87c05ce38921069cab00d5711'
