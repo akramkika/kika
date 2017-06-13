a78b11b690c0d0977332007d81b56baacee86e25
theohugues <theo.hugues@gmail.com>
2017-06-13 02:19:56 +0200

    fix eslint

--- spotify.js
+++ spotify.js
@@ -5,48 +5,39 @@ var spotifyApi = new SpotifyWebApi({
   clientId: clientid,
   clientSecret: clientSecret
 })
-var result
-var type
 
 module.exports = {
   spotify: function (msg) {
     if (msg.content.lastIndexOf('!spotify') !== -1) {
       spotifyApi.clientCredentialsGrant()
 
-
-
       // TOKEN
       .then(function (data) {
-
-        console.log(' Le token expire le : ' + data.body['expires_in'])
-        console.log('Voici le token : ' + data.body['access_token'])
+        // console.log(' Le token expire le : ' + data.body['expires_in'])
+        // console.log('Voici le token : ' + data.body['access_token'])
 
         spotifyApi.setAccessToken(data.body['access_token'])
-        result = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
+        // var result = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
 
         if (msg.content.endsWith('--alb')) {
-          type = 0
-        }
-        else if (msg.content.endsWith('--art')) {
+          var type = 0
+        } else if (msg.content.endsWith('--art')) {
           type = 1
-        }
-        else if (msg.content.endsWith('--tra')) {
+        } else if (msg.content.endsWith('--tra')) {
           type = 2
-        }
-        else {
+        } else {
           type = 2
         }
 
         if (msg.content.endsWith('--art') || msg.content.endsWith('--tra') || msg.content.endsWith('--alb')) {
-           var a = (msg.content).length
-           var b = a - 5
-           var asw = msg.content.substring(9, b)
-        }
-        else {
-           asw = msg.content.substring(9)
+          var a = (msg.content).length
+          var b = a - 5
+          var asw = msg.content.substring(9, b)
+        } else {
+          asw = msg.content.substring(9)
         }
 
-      if (type == 0) {
+        if (type === 0) {
           // ALBUMS
           spotifyApi.searchTracks('album:' + asw)
           .then(function (data) {
@@ -54,9 +45,9 @@ module.exports = {
             for (var i = 0; i < 3; i++) {
               msg.channel.sendMessage('"' + data.body.tracks.items[i].album.name + '" de ' + data.body.tracks.items[i].artists[0].name)
             }
-        })
-}
-        if (type == 1) {
+          })
+        }
+        if (type === 1) {
           // ARTISTES
           spotifyApi.searchArtists(asw)
           .then(function (data) {
@@ -66,8 +57,7 @@ module.exports = {
             }
           })
         }
-          if (type == 2) {
-
+        if (type === 2) {
           // TRACKS
           spotifyApi.searchTracks('track:' + asw)
           .then(function (data) {
@@ -77,14 +67,8 @@ module.exports = {
             }
           })
         }
-
-
-
-msg.channel.sendMessage('Vous pouvez affinez votre recherche avec --alb --art --tra pour rechercher un album, un artiste ou une track.')
-
+        msg.channel.sendMessage('Vous pouvez affinez votre recherche avec --alb --art --tra pour rechercher un album, un artiste ou une track.')
       })
-
     }
-
   }
 }
