6df28a90a77dc8ac0c45c20154f7d32126ab49a9
Al-Eiks <alexis.santangeli@gmail.com>
2017-06-13 02:48:18 +0200

    Corrections

--- services/twitter.js
+++ services/twitter.js
@@ -3,22 +3,21 @@ var twitterclient = new Twitter(require('../config.js'))
 
 module.exports = {
   sendTweet: function (msg) {
-    if (msg.content.startsWith('!tweet ') !== -1) {
+    if (msg.content.startsWith('!tweet ')) {
       if (msg.content.length <= 146) {
         twitterclient.post('statuses/update', {status: msg.content.substring(6)}, function (error, tweet, response) {
           if (error) {
             console.log(error)
             throw error
           }
-          // console.log(tweet)
-          // console.log(response)
+          msg.channel.send('Tweet envoyé !')
         })
       } else {
-        msg.channel.sendMessage('Le tweet est trop loooooooooong mamene...')
+        msg.channel.send('Le tweet est trop loooooooooong...')
       }
     }
   },
-  listenAccount: function () {
+  listenAccount: function (msg) {
     twitterclient.stream('statuses/filter', {track: '#botweet_jat'}, function (stream) {
       stream.on('data', function (tweet) {
         console.log(tweet.text)
